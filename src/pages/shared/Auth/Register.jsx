d0bc61ca02125b6d41ddfa187582dabe0851ccca
Tariful Hasan <tarif305@gmail.com>
2026-02-25 16:36:11 +0600

    registration and firebase added

--- src/pages/shared/Auth/Register.jsx
+++ src/pages/shared/Auth/Register.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleRegistration = (data) => {
+    console.log("after register", data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleRegistration)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p role="alert" className="text-red-500">
+              Email is required
+            </p>
+          )}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              pattern:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+            })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">Password must be 6 character</p>
+          )}
+          {
+            errors.password?.type === 'pattern' && (<p className="text-red-500">Password must have one uppercase, one lowercase and a special character</p>)
+          }
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
