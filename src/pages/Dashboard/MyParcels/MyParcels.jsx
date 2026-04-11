0c323a9bf9f4ff53db70a80c25bd9574c8a432d6
Tariful Hasan <tarif305@gmail.com>
2026-04-11 22:20:31 +0600

    my parcel on progress

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -7,7 +7,7 @@ const MyParcels = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const {data: parcels = []} = useQuery({
+  const { data: parcels = [] } = useQuery({
     queryKey: ["myParcels", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user.email}`);
@@ -18,9 +18,34 @@ const MyParcels = () => {
   return (
     <div>
       <h2>All of my parcels : {parcels.length}</h2>
+
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Weight</th>
+              <th>Cost</th>
+              <th>Payment Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parcels.map((parcels, index) => (
+              <tr key={parcels.id}>
+                <th>{index + 1}</th>
+                <td>{parcels.parcelName}</td>
+                <td>{parcels.parcelWeight}</td>
+                <td>{parcels.parcelCost}</td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
 
 export default MyParcels;
-  
