a13d37ea79f41fdd91b4e40f40ec2f7d6f017e93
Tariful Hasan <tarif305@gmail.com>
2026-04-14 11:56:48 +0600

    payemnt pathway done

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../../Hook/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const Payment = () => {
-  return (
-    <div>Payment</div>
-  )
-}
+  const { parcelId } = useParams();
+  const axiosSecure = useAxiosSecure();
 
-export default Payment
+  const { isLoading, data: parcel } = useQuery({
+    queryKey: ["parcels", parcelId],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels/${parcelId}`);
+      return res.data;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <div>
+        <span className="loading loading-infinity loading-xl"></span>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>Please Pay for : {parcel.parcelName}</h2>
+      <button className="btn btn-primary text-black ">Pay</button>
+    </div>
+  );
+};
+
+export default Payment;
