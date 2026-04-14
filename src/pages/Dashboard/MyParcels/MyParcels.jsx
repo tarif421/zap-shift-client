0c5a41ac25a1c4366a52a1ba6f6c44228433fa79
Tariful Hasan <tarif305@gmail.com>
2026-04-14 22:21:10 +0600

    payment updated

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -48,6 +48,21 @@ const MyParcels = () => {
     });
   };
 
+  const handlePayment = async (parcel) => {
+    const paymentInfo = {
+      cost: parcel.cost,
+      parcelId: parcel._id,
+      senderEmail: parcel.senderEmail,
+      parcelName: parcel.parcelName,
+    };
+    const res = await axiosSecure.post(
+      "/payment-checkout-session",
+      paymentInfo,
+    );
+    console.log(res.data.url);
+    window.location.href = res.data.url;
+  };
+
   return (
     <div>
       <h2>All of my parcels : {parcels.length}</h2>
@@ -75,11 +90,12 @@ const MyParcels = () => {
                   {parcels.paymentStatus === "paid" ? (
                     <span className="text-green-400">Paid</span>
                   ) : (
-                    <Link to={`/dashboard/payment/${parcels._id}`}>
-                      <button className="btn btn-sm btn-primary text-black">
-                        Pay
-                      </button>
-                    </Link>
+                    <button
+                      onClick={() => handlePayment(parcels)}
+                      className="btn btn-sm btn-primary text-black"
+                    >
+                      Pay
+                    </button>
                   )}
                 </td>
                 <td>{parcels.cost}</td>
