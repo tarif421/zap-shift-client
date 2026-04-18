ec42c6b8af3c6757720d4761d7a5f1982c34d331
Tariful Hasan <tarif305@gmail.com>
2026-04-18 14:33:38 +0600

    token verify done

--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -2,20 +2,47 @@ import axios from "axios";
 import React from "react";
 import useAuth from "./useAuth";
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
   useEffect(() => {
     // intercept request
-    axiosSecure.interceptors.request.use((config) => {
-      config.headers.Authorization = `Bearer ${user?.accessToken}`
-      return config
-    })
-  }, [user])
+    const reqItercepter = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+
+    //  interceptor response
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        // console.log("Success Response came!", response.data);
+        return response;
+      },
+      (error) => {
+        // console.log("Interceptor detected an error:", error.response);
+        // unauthorized ba forbidded hole logout
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          logOut().then(() => {
+            navigate("/login");
+          });
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(reqItercepter);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user]);
 
   return axiosSecure;
 };
