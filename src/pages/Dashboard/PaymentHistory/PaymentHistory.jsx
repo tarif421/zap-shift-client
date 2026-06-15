f1b1af745f947532eff8d6431adf605864a14797
Tariful Hasan <tarif305@gmail.com>
2026-06-15 22:30:21 +0600

    payment history table added

--- src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -6,16 +6,39 @@ import useAxiosSecure from "../../../Hook/useAxiosSecure";
 const PaymentHistory = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const {data: payments = []} = useQuery({
+  const { data: payments = [] } = useQuery({
     queryKey: ["payments", user.email],
     queryFn: async () => {
-        const res = await axiosSecure.get(`/payments?email=${user.email}`)
-        return res.data
+      const res = await axiosSecure.get(`/payments?email=${user.email}`);
+      return res.data;
     },
   });
   return (
     <div>
-      <h2 className="text-5xl">Payment History:  {payments.length}</h2>
+      <h2 className="text-5xl">Payment History: {payments.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {payments.map((payment, index) => (
+              <tr key={payment._id}>
+                <th>{index + 1}</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
