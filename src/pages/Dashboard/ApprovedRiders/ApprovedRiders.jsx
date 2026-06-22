e489b5d498802361b94c6ef03f927413f84be3a5
Tariful Hasan <tarif305@gmail.com>
2026-06-22 16:57:47 +0600

    rider role update

--- src/pages/Dashboard/ApprovedRiders/ApprovedRiders.jsx
+++ src/pages/Dashboard/ApprovedRiders/ApprovedRiders.jsx
@@ -4,20 +4,39 @@ import useAxiosSecure from "../../../Hook/useAxiosSecure";
 import { FaUserCheck } from "react-icons/fa";
 import { IoPersonRemove } from "react-icons/io5";
 import { FaTrashCan } from "react-icons/fa6";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const ApprovedRiders = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: riders = [] } = useQuery({
+  const { refetch, data: riders = [] } = useQuery({
     queryKey: ["riders", "pending"],
     queryFn: async () => {
       const res = await axiosSecure.get("/riders");
       return res.data;
     },
   });
+  const updateRiderStatus = (rider, status) => {
+    const updateInfo = { status: status, email: rider.email };
+    axiosSecure.patch(`/riders/${rider._id}`, updateInfo).then((res) => {
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `Rider status is set to been ${status}`,
 
-  const handleApproval = () => {
-    
-  }
+          confirmButtonColor: "#16a34a",
+        });
+      }
+    });
+  };
+  const handleApproval = (rider) => {
+    updateRiderStatus(rider, "approved");
+  };
+  const handleRejection = (rider) => {
+    updateRiderStatus(rider, "rejected");
+  };
   return (
     <div>
       <h2 className="text-5xl">Riders Pending Approval {riders.length}</h2>
@@ -36,17 +55,28 @@ const ApprovedRiders = () => {
           </thead>
           <tbody>
             {riders.map((rider, index) => (
-              <tr>
+              <tr key={rider._id}>
                 <th>{index + 1}</th>
                 <td>{rider.name}</td>
                 <td>{rider.email}</td>
                 <td>{rider.district}</td>
-                <td>{rider.status}</td>
+
                 <td>
-                  <button onClick={() => handleApproval(rider._id)} className="btn">
+                  <p
+                    className={`${rider.status === "approved" ? "text-green-800" : "text-red-500"}`}
+                  >
+                    {rider.status || 'pending'}
+                  </p>
+                </td>
+
+                <td>
+                  <button onClick={() => handleApproval(rider)} className="btn">
                     <FaUserCheck />
                   </button>
-                  <button className="btn">
+                  <button
+                    onClick={() => handleRejection(rider)}
+                    className="btn"
+                  >
                     <IoPersonRemove />
                   </button>
                   <button className="btn">
