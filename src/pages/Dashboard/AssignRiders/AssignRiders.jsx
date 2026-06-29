dcf9db7543663a70c21e8690d9adfadd70e728ce
Tariful Hasan <tarif305@gmail.com>
2026-06-29 12:23:17 +0600

    assgin riders done

--- src/pages/Dashboard/AssignRiders/AssignRiders.jsx
+++ src/pages/Dashboard/AssignRiders/AssignRiders.jsx
@@ -1,12 +1,14 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import useAxiosSecure from "../../../Hook/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import { FaUserCheck } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AssignRiders = () => {
+  const [selectedParcel, setSelectedParcel] = useState(null);
   const axiosSecure = useAxiosSecure();
   const riderModalRef = useRef();
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch: parcelsRefetch } = useQuery({
     queryKey: ["parcels", "pending-pickup"],
     queryFn: async () => {
       const res = await axiosSecure.get(
@@ -15,9 +17,44 @@ const AssignRiders = () => {
       return res.data;
     },
   });
-
+//  todo: invalidate query after assign a rider
+  const { data: riders = [] } = useQuery({
+    queryKey: ["riders", selectedParcel?.senderDistrict, "available"],
+    enabled: !!selectedParcel,
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/riders?status=approved&available&district=${selectedParcel?.senderDistrict}&workStatus=available`,
+      );
+      return res.data;
+    },
+  });
   const openAssignRiderModal = (parcel) => {
+    setSelectedParcel(parcel);
     riderModalRef.current.showModal();
+  };
+
+  const handleAssignRider = (rider) => {
+    const riderAssignInfo = {
+      riderId: rider._id,
+      riderEmail: rider.email,
+      riderName: rider.name,
+      parcelId: selectedParcel._id,
+    };
+    axiosSecure
+      .patch(`/parcels/${selectedParcel._id}`, riderAssignInfo)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          riderModalRef.current.close();
+          parcelsRefetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `Rider has been assigned `,
+            showConfirmButton: false,
+            timer: 2000,
+          });
+        }
+      });
   };
   return (
     <div>
@@ -49,16 +86,8 @@ const AssignRiders = () => {
                     className="btn btn-primary text-black"
                   >
                     <FaUserCheck className="text-base" />
-                    Assign Rider
+                    Find Riders
                   </button>
-                </td>
-
-                <td>
-                  <p
-                    className={`${parcel.status === "approved" ? "text-green-800" : "text-red-500"}`}
-                  >
-                    {parcel.status || "pending"}
-                  </p>
                 </td>
               </tr>
             ))}
@@ -72,10 +101,41 @@ const AssignRiders = () => {
         className="modal modal-bottom sm:modal-middle"
       >
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
+          <h3 className="font-bold text-lg">Riders: {riders.length}</h3>
+          <div className="overflow-x-auto">
+            <table className="table table-zebra">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Job</th>
+                  <th>Favorite Color</th>
+                </tr>
+              </thead>
+              <tbody>
+                {riders.map((rider, i) => (
+                  <tr key={rider._id}>
+                    
+                    <th>{i + 1}</th>
+                    <td>{rider.name}</td>
+                    <td>{rider.email}</td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          handleAssignRider(rider);
+                        }}
+                        className="btn btn-primary text-black"
+                      >
+                        Assign
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
--- src/pages/Dashboard/ManageUser/ManageUser.jsx
+++ src/pages/Dashboard/ManageUser/ManageUser.jsx
@@ -6,7 +6,7 @@ import Swal from "sweetalert2";
 
 const ManageUser = () => {
   const axiosSecure = useAxiosSecure();
-  const [searchText, setSearchText] = useState();
+  const [searchText, setSearchText] = useState("");
 
   const { data: users = [], refetch } = useQuery({
     queryKey: ["users", searchText],
