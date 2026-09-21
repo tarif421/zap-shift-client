ad2437be444d7eadee2c191b10735c97ffb4748b
Tariful Hasan <tarif305@gmail.com>
2026-09-21 07:39:42 +0600

    assigned tast accepted

--- src/pages/Dashboard/RiderBoard/AssignedDeliveries.jsx
+++ src/pages/Dashboard/RiderBoard/AssignedDeliveries.jsx
@@ -2,20 +2,38 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../../../Hook/useAuth";
 import useAxiosSecure from "../../../Hook/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AssignedDeliveries = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["parcels", user.email, "driver_assigned"],
     queryFn: async () => {
       const res = await axiosSecure.get(
-        `/parcels/rider?riderEmail=${user.email}&deliveryStatus=driver_assigned`,
+        `/parcels/rider?riderEmail=${user?.email}&deliveryStatus=driver_assign`,
       );
       console.log(res.data);
       return res.data;
     },
   });
+  const handleAcceptDelivery = (parcel) => {
+    const statusInfo = { deliveryStatus: "rider_arriving" };
+    axiosSecure
+      .patch(`/parcels/${parcel._id}/status`, statusInfo)
+      .then((res) => {
+        refetch();
+        if (res.data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Thank you for Accepting",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   return (
     <div>
       <h2 className="text-4xl">Parcels Pending Pickup: {parcels.length}</h2>
@@ -26,7 +44,7 @@ const AssignedDeliveries = () => {
             <tr>
               <th></th>
               <th>Name</th>
-              <th>Address</th>
+              <th>Confirm</th>
               <th>Phone</th>
             </tr>
           </thead>
@@ -35,7 +53,23 @@ const AssignedDeliveries = () => {
               <tr>
                 <th>{index + 1}</th>
                 <td>{parcel.parcelName}</td>
-                <td>{parcel.deliveryAddress}</td>
+                <td>
+                  {parcel.deliveryStatus === "driver_assign" ? (
+                    <>
+                      <button
+                        onClick={() => handleAcceptDelivery(parcel)}
+                        className="btn btn-primary text-black"
+                      >
+                        Accept
+                      </button>
+                      <button className="btn btn-warning text-black ml-3">
+                        Reject
+                      </button>
+                    </>
+                  ) : (
+                    <span className="btn btn-primary text-black">Accepted</span>
+                  )}
+                </td>
                 <td>{parcel.customerPhone}</td>
               </tr>
             ))}
